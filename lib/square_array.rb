71f222ed566617125a24ee6f41eb76269bfb7851
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-09 10:37:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
 
   while counter < array.length do
     integer = array[counter]
-    new_array.push(Math.sqrt(integer))
+    new_array.push(Math.sqrt(2))
     counter += 1
 
   end
